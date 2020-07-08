fa4d2bfc9180f07f94d162e0706a3238710b0fa5
Phong Vo <vhphong>
2020-07-08 00:24:44 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MySQL_CShrap_XAMPP_Winforms
+{
+	partial class FormUI
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtCmdBox = new System.Windows.Forms.TextBox();
+			this.btnExecQuery = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtCmdBox
+			// 
+			this.txtCmdBox.Location = new System.Drawing.Point(12, 12);
+			this.txtCmdBox.Multiline = true;
+			this.txtCmdBox.Name = "txtCmdBox";
+			this.txtCmdBox.Size = new System.Drawing.Size(789, 171);
+			this.txtCmdBox.TabIndex = 0;
+			// 
+			// btnExecQuery
+			// 
+			this.btnExecQuery.Location = new System.Drawing.Point(807, 12);
+			this.btnExecQuery.Name = "btnExecQuery";
+			this.btnExecQuery.Size = new System.Drawing.Size(163, 61);
+			this.btnExecQuery.TabIndex = 1;
+			this.btnExecQuery.Text = "Executive Query";
+			this.btnExecQuery.UseVisualStyleBackColor = true;
+			this.btnExecQuery.Click += new System.EventHandler(this.btnExecQuery_Click);
+			// 
+			// FormUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(982, 556);
+			this.Controls.Add(this.btnExecQuery);
+			this.Controls.Add(this.txtCmdBox);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+			this.Name = "FormUI";
+			this.Text = "MySQL C# XAMPP Winforms";
+			this.Load += new System.EventHandler(this.FormUI_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtCmdBox;
+		private System.Windows.Forms.Button btnExecQuery;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_CShrap_XAMPP_Winforms
+{
+	public partial class FormUI : Form
+	{
+		public FormUI()
+		{
+			InitializeComponent();
+		}
+
+		private void FormUI_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnExecQuery_Click(object sender, EventArgs e)
+		{
+			runQuery();
+		}
+
+		private void runQuery()
+		{
+			string query = txtCmdBox.Text;
+
+			if (query == "")
+			{
+				MessageBox.Show("Please insert a sql query!");
+				return;
+			}
+
+			string MySQLConnectionString = "datasource=127.0.0.1; port=3306; username=root; password=; database=dbstudentmanager";
+
+			MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+			MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+			commandDatabase.CommandTimeout = 60;
+
+			try
+			{
+				databaseConnection.Open();
+
+				MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+				if (myReader.HasRows)
+				{
+					MessageBox.Show("Your query generated results, please see the console");
+
+					while (myReader.Read())
+					{
+						Console.WriteLine(myReader.GetString(0) + " - " +		// sid
+										  myReader.GetString(1) + " - " +		// firstname
+										  myReader.GetString(2) + " - " +		// lastname
+										  myReader.GetString(3) + " - " +		// ssn
+										  myReader.GetString(4) + " - " +		// dob
+										  myReader.GetString(5) + " - " +		// gender
+										  myReader.GetString(6) + " - " +		// race
+										  myReader.GetString(7) + " - " +		// photo filename
+										  myReader.GetString(8)					// submissions filenames
+										 );
+					}
+				}
+				else
+				{
+					MessageBox.Show("Query successfully executed");
+				}
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("Query error " + e.Message);
+			}
+
+
+		}
+	}
+}
